8747c4d060e87d9fe433a829a24408d4c9255ac8
Jovan <suvajacjovan@gmail.com>
2020-05-05 10:43:01 -0400

    build script fix for multiple src files

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,8 @@ mkdir build
 xcopy.exe lib\SDL\lib\win64\SDL2.dll build
 pushd build
 cl -EHsc -Zi -FC^
-    ..\src\engine.cpp ..\lib\GLAD\src\glad.c^
+    ..\src\* ..\lib\GLAD\src\glad.c^
+	-Fe:engine.exe^
     -I ../lib/SDL/include -I ../lib/GLAD/include -I ../include^
     Shell32.lib SDL2.lib opengl32.lib^
     -link -LIBPATH:../lib/SDL/lib/win64 -SUBSYSTEM:CONSOLE
